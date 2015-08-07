26020a4512248dae6d9088b14aafcb164979d276
jingbiaowang <wwwwjb123@126.com>
2015-08-07 16:35:44 +0800

    add main activity

---
+++ app/src/main/java/com/sohu/live56/view/main/square/live/MainActivity.java
@@ -0,0 +1,22 @@
+package com.sohu.live56.view.main.square.live;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.sohu.live56.R;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
