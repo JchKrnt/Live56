450c998dc913c98c40c871f847d5b9c2c77eb260
jingchuanhu <wwwwjb132@126.com>
2015-08-11 07:52:53 +0800

    Merge branch 'master' of https://github.com/JchKrnt/Live56
    Conflicts:
    	.idea/misc.xml

--- app/src/main/java/com/sohu/live56/view/main/MainActivity.java
+++ app/src/main/java/com/sohu/live56/view/main/MainActivity.java
@@ -1,11 +1,7 @@
 package com.sohu.live56.view.main;
 
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
 
 import com.sohu.live56.R;
 
