08c2c33d628709c41021833f1ac2279481ea458c
jingbiaowang <wwwwjb123@126.com>
2015-08-10 20:24:45 +0800

    add tabhost Bar

---
+++ app/src/main/java/com/sohu/live56/util/LogCat.java
@@ -0,0 +1,50 @@
+package com.sohu.live56.util;
+
+import android.util.Log;
+
+/**
+ * Created by jchsohu on 15-7-6.
+ */
+public class LogCat {
+
+    private static boolean releaseAble = false;
+
+    private static final String TAG = "SOHU_LIVE";
+
+    public static void setRleaseable(boolean release) {
+        releaseAble = release;
+    }
+
+    public static void debug(String msg) {
+        if (releaseAble) {
+            return;
+        } else
+            Log.d(TAG, msg);
+    }
+
+    public static void e(String msg) {
+        if (releaseAble) {
+            return;
+        } else
+            Log.e(TAG, msg);
+    }
+
+    public static void e(String tag, String msg) {
+
+        Log.e(tag, msg);
+    }
+
+    public static void v(String msg) {
+        if (releaseAble) {
+            return;
+        } else
+            Log.v(TAG, msg);
+    }
+
+    public static void i(String msg) {
+        if (releaseAble) {
+            return;
+        } else
+            Log.i(TAG, msg);
+    }
+}
