fac7d93066763144a3bc6775fa0f322485835e76
jingbiaowang <wwwwjb123@126.com>
2015-08-17 19:40:16 +0800

    layout ovbserver view

--- app/src/main/java/com/sohu/live56/view/login/PersonalSettingActivity.java
+++ app/src/main/java/com/sohu/live56/view/login/PersonalSettingActivity.java
@@ -4,36 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.sohu.live56.R;
+import com.sohu.live56.view.BaseTitleActivity;
 
-public class PersonalSettingActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personal_setting);
-    }
+public class PersonalSettingActivity extends BaseTitleActivity {
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_personal_setting, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected View onAddContainerView() {
+        return null;
     }
 }
